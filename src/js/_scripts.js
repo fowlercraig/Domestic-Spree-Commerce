b6a6f206124664e5d97fa3afbd44eb73599cd910
Craig Fowler <craig@wearegiants.com>
2015-04-28 20:28:18 -0700

    Added junk.

---
+++ src/js/_scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  // Packery
+
+  var $container = $('.packery-grid');
+
+  $container.packery({
+    itemSelector: '.fs-cell-contained',
+    gutter: 0
+  });
+
+
+});
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+// Formstone Components
+
+// @codekit-prepend "../bower_components/formstone/dist/js/core.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/touch.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/transition.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/dropdown.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/mediaquery.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/swap.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/navigation.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/cookie.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/carousel.js"
+// @codekit-prepend "../bower_components/formstone/dist/js/equalize.js"
+
+// Other Plugins
+
+// @codekit-prepend "../bower_components/packery/dist/packery.pkgd.js"
+// @codekit-prepend "../bower_components/magnific-popup/dist/jquery.magnific-popup.min.js"
+
+// @codekit-append "_scripts.js"
